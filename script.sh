175bf9a8c94b0e20ee993f6dc5b85ea011176901
Jacky Alvarado <jackat91@gmail.com>
2018-02-05 16:16:49 +0100

    script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-#docker network create exnet3
+docker network create exnet3
 
-docker create -v /var/lib/mysql -v /usr/share/nginx/html --network exnet3 --name datacontainer datacontainer-nginx-db 
+docker create -v /var/lib/mysql -v /usr/share/nginx/html --network exnet3 --name datacontainer jackyalt/datacontainer-nginx-db 
 
 docker run --name db -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE1=wordpress -e MYSQL_USER1=wpress -e MYSQL_PASSWORD1=wpress --network exnet3 --volumes-from datacontainer orboan/dcsss-mariadb
 
