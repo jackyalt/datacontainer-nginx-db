d3fac78ffbfcf95a004787c4d4a531dd8fc31f1e
Jacky Alvarado <jackat91@gmail.com>
2018-02-05 16:14:02 +0100

    my script

---
+++ script.sh
@@ -0,0 +1,7 @@
+#docker network create exnet3
+
+docker create -v /var/lib/mysql -v /usr/share/nginx/html --network exnet3 --name datacontainer datacontainer-nginx-db 
+
+docker run --name db -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE1=wordpress -e MYSQL_USER1=wpress -e MYSQL_PASSWORD1=wpress --network exnet3 --volumes-from datacontainer orboan/dcsss-mariadb
+
+docker run -d -p 8080:80 --network exnet3 --volumes-from datacontainer --name nginx-php wyveo/nginx-php-fpm 
